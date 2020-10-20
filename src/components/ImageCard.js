2d21ef9a6b7aa44147dea8225fed828c3c47ed62
Ben Pluim <bpluim@Administrators-MacBook-Pro.local>
2020-10-20 15:51:07 -0600

    image cards

---
+++ src/components/ImageCard.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+
+export class ImageCard extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.imageRef = React.createRef();
+    }
+
+    componentDidMount() {
+        this.imageRef.current.addEventListener('load', this.setSpans);
+    }
+
+    setSpans() {
+        
+    }
+
+    render() {
+
+        const { description, urls } = this.props.image;
+
+        return (
+            <div>
+                <img 
+                    ref={this.imageRef}
+                    alt={description} 
+                    src={urls.regular}
+                />
+            </div>
+        )
+    }
+}
+
+export default ImageCard
